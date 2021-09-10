7a0cf0903fbaee1d8ebe7e43fd55416c6c6e8558
SupplyChef <47842426+SupplyChef@users.noreply.github.com>
2021-09-10 11:18:45 -0700

    Setting up documentation

--- docs/make.jl
+++ docs/make.jl
@@ -13,9 +13,6 @@ makedocs(
             ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(;
+    repo="https://github.com/SupplyChef/SupplyChainOptimization.jl",
+)
